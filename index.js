bed7596c2bf865e3d36c8396929fbc9bdb4d34c3
oregonstatetm <35415571+oregonstatetm@users.noreply.github.com>
2020-11-01 19:52:18 -0800

    code for initial app

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req,res) => {
+    res.render('home.ejs')
+})
+
+
+app.listen(3000, () => {
+    console.log("Listening on Port 3000")
+
+})
