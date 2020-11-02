bdf2f350038a1e706f7c9cd5ee046c8f6c4abbc7
oregonstatetm <35415571+oregonstatetm@users.noreply.github.com>
2020-11-01 20:07:34 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
+
 
 app.set('view engine', 'ejs');
 
@@ -7,10 +9,6 @@ app.get('/', (req,res) => {
     res.render('home.ejs')
 })
 
-/*
-app.listen(3000, () => {
-    console.log("Listening on Port 3000")
-
+app.listen(port, function() {
+    console.log("Server started");
 })
-
-*/
