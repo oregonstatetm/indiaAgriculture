a3c480f66dbe381089ff67ef8667835e01ebc8ee
esanchit <chopras@DESKTOP-FFJOPGI.localdomain>
2020-11-20 20:00:22 +0530

    Adding for Inserting Into Orders Table

--- dbcon.js
+++ dbcon.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
 	connectionLimit : 10,
 	host            : 'classmysql.engr.oregonstate.edu',
-	user            : 'cs_340chopras',
+	user            : 'cs340_chopras',
 	password        : '1227',
 	database        : 'cs340_chopras'
 });
