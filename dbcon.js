82f5927f2d1422036638c3db7cc66156dd7592a8
oregonstatetm <35415571+oregonstatetm@users.noreply.github.com>
2020-12-06 22:25:00 -0800

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
 	connectionLimit : 10,
-	host            : 'classmysql.engr.oregonstate.edu',
-	user            : 'cs340_chopras',
-	password        : '1227',
-	database        : 'cs340_chopras',
+	host            : 'us-cdbr-east-02.cleardb.com',
+	user            : 'b46a461f5e3a4c',
+	password        : '7c95039b',
+	database        : 'heroku_9a471125d48dd35',
 });
 module.exports.pool = pool;
